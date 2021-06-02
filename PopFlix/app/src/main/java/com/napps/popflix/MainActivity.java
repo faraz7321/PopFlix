a17ba669bda6be2b371b3d4d62e4815d3459ffcf
faraz7321 <81792548+faraz7321@users.noreply.github.com>
2021-06-02 21:00:46 +0500

    Splashy implemented
    Problem with the data transferring persist. Tried inheritance and static methods to throw movies data

--- PopFlix/app/src/main/java/com/napps/popflix/MainActivity.java
+++ PopFlix/app/src/main/java/com/napps/popflix/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Setting Dynmaic UI Color
+        // Setting dynamic UI Color
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
         @SuppressLint("CutPasteId") View someView = findViewById(R.id.MovieView);
--- PopFlix/app/src/main/java/com/napps/popflix/models/DetailActivity.java
+++ PopFlix/app/src/main/java/com/napps/popflix/models/DetailActivity.java
@@ -1,7 +1,20 @@
 package com.napps.popflix.models;
 
-public class DetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.napps.popflix.R;
+
+public class DetailActivity extends AppCompatActivity {
     String MovieTitle;
     String MovieOverview;
     double MovieRating;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        //implementation
+    }
 }
