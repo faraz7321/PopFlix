2c742d7926baa78077f2acf222735cd20f302e76
faraz7321 <81792548+faraz7321@users.noreply.github.com>
2021-06-03 11:06:24 +0500

    Update DetailActivity.java
    Changed the data types of its attributes from string to textview and ratingbar
    (android widgets)

--- PopFlix/app/src/main/java/com/napps/popflix/models/DetailActivity.java
+++ PopFlix/app/src/main/java/com/napps/popflix/models/DetailActivity.java
@@ -2,19 +2,41 @@ package com.napps.popflix.models;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.napps.popflix.R;
 
+import org.parceler.Parcels;
+
 public class DetailActivity extends AppCompatActivity {
-    String MovieTitle;
-    String MovieOverview;
-    double MovieRating;
+
+    TextView MovieTitle;
+    TextView MovieOverview;
+    RatingBar MovieRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        //implementation
+        // Setting Dynmaic UI Color
+        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_detail);
+        @SuppressLint("CutPasteId") View someView = findViewById(R.id.linearLayout);
+        View root = someView.getRootView();
+        root.setBackgroundColor(getResources().getColor(R.color.UIblack));
+        @SuppressLint("CutPasteId") LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        MovieTitle = findViewById(R.id.DetailedMovieTitle);
+        MovieOverview = findViewById(R.id.DetailedMovieOver);
+        MovieRating = findViewById(R.id.MovieRating);
+        Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        MovieTitle.setText(movie.getTitle());
+        MovieOverview.setText(movie.getOverview());
+        MovieRating.setRating((float) movie.getRating());
+        // parse out extra additional data post
     }
 }
